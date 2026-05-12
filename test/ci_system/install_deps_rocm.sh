663eccebbfc6e31f20f530131ad51105f27a012c
LightSeek Foundation <243258330+lightseek-bot@users.noreply.github.com>
2026-05-12 14:51:12 -0700

    Apply suggestion from @lightseek-bot

--- test/ci_system/install_deps_rocm.sh
+++ test/ci_system/install_deps_rocm.sh
@@ -37,7 +37,7 @@ pip3 install tokenspeed-scheduler/
 
 echo "=== Step 5: Install TokenSpeed ==="
 pip3 install smg smg-grpc-servicer smg-grpc-proto \
-    --extra-index-url https://lightseek.org/whl/cu130
+    --extra-index-url https://lightseek.org/whl/rocm72
 pip3 install -e ./python --no-build-isolation \
     --extra-index-url "${ROCM_INDEX}"
 
