8cd99d9fd6ed14dfbf9fa58b574b467a51730626
tuanzhangCS <tuan@lightseek.org>
2026-05-21 20:05:58 +0800

    opt: fuse in_proj_qkvz and in_proj_ba to one gemm.
    opt: remove contiguous in mamba attention.
    
    fix weight load bug.
    
    fix error
    
    opt: fuse qk_rmsnorm_rope_gate kernel.
    
    update, fix bug

--- python/tokenspeed/runtime/layers/attention/backends/hybrid_linear_attn.py
+++ python/tokenspeed/runtime/layers/attention/backends/hybrid_linear_attn.py
@@ -886,10 +886,11 @@ class MambaAttnBackend(AttentionBackend):
             output_indices = self.forward_metadata.mamba_output_indices
 
             batch_size = seq_len // draft_token_num
+            # shouldn't use contiguous here, because causal_conv1d_update
+            # support input non-contiguous
             mixed_qkv_reshaped = (
                 mixed_qkv.view(batch_size, draft_token_num, -1)
                 .transpose(1, 2)
-                .contiguous()
             )
             mixed_qkv_processed = causal_conv1d_update(
                 mixed_qkv_reshaped,
@@ -900,8 +901,9 @@ class MambaAttnBackend(AttentionBackend):
                 conv_state_indices=cache_indices[:batch_size],
                 output_state_indices=output_indices[:batch_size],
             )
+            # needn't contiguous here.
             mixed_qkv = (
-                mixed_qkv_processed.transpose(1, 2).contiguous().view(seq_len, -1)
+                mixed_qkv_processed.transpose(1, 2).view(seq_len, -1)
             )
         else:
             conv_states, ssm_states = self.pool.get_mamba_params(layer_id)
