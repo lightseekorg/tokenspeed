f388a28fad374476c56bc4be2fd4089997eaa69e
Pengzhan Zhao <borontion@gmail.com>
2026-06-08 21:41:03 -0700

    update script
    Signed-off-by: Pengzhan Zhao <borontion@gmail.com>

--- test/random_benchmark/tokenspeed/collect_outputs.py
+++ test/random_benchmark/tokenspeed/collect_outputs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Collect GPT-OSS random perf sweeps into one CI summary table."""
+"""Collect random perf sweeps into one CI summary table."""
 
 import argparse
 import csv
