700ea445162d3b2f52d914ffa1994f6e1ba559a7
Yue Weng <25103990+yweng0828@users.noreply.github.com>
2026-09-03 10:18:54 +0000

    refactor speculative-num-step setting
    Signed-off-by: Yue Weng <25103990+yweng0828@users.noreply.github.com>

--- test/runtime/test_deepseek_v4_config.py
+++ test/runtime/test_deepseek_v4_config.py
@@ -1417,6 +1417,7 @@ class TestDeepseekV4Config(unittest.TestCase):
             mapping=None,
             speculative_algorithm="DSPARK",
             enable_prefix_caching=True,
+            speculative_num_steps=5,
             speculative_num_draft_tokens=6,
             prefix_granularity=256,
             load_format="auto",
