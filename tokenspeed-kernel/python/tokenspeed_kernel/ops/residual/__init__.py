b638d084ddfef29979113f7b7fadf5c3966e8710
Pengzhan Zhao <borontion@gmail.com>
2026-09-09 21:20:21 -0700

    Cleanup
    Signed-off-by: Pengzhan Zhao <borontion@gmail.com>

--- tokenspeed-kernel/python/tokenspeed_kernel/ops/residual/__init__.py
+++ tokenspeed-kernel/python/tokenspeed_kernel/ops/residual/__init__.py
@@ -18,8 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
-"""Residual-routing kernel entry points."""
-
 from __future__ import annotations
 
 import math
